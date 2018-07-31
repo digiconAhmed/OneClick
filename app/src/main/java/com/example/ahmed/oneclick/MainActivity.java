564346b6e4a6783aa01aa7314425fb53d9d8e9cc
Ahmed <muhammedahmedtariq.786@gmail.com>
2018-07-31 12:46:44 +0500

    ahmed adding splash

--- app/src/main/java/com/example/ahmed/oneclick/MainActivity.java
+++ app/src/main/java/com/example/ahmed/oneclick/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ahmed.oneclick;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/ahmed/oneclick/Splash.java
@@ -0,0 +1,24 @@
+package com.example.ahmed.oneclick;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+    private static int Splash_time_out=5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(Splash.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },Splash_time_out);
+    }
+}
